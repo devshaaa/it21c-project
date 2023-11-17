edd88d2cacb9c9f50da2f94c43a400ffdc5e653a
Xeirah07 <suyatsarah519@gmail.com>
2023-11-17 13:49:28 +0800

    E commit ang inheritance hehe

---
+++ src/inheritance/Kyrie.java
@@ -0,0 +1,14 @@
+
+package inheritance;
+
+public class Kyrie {
+     String name;
+    
+    Kyrie (String name){
+        this.name = name;
+        
+    }
+    void hard(){
+        System.out.println(name + " maybe challenging, but remember, storms don't last. ");
+    }
+}
